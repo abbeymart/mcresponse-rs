2a22b2057631b51af6bd3977161aa206dcd49a72
Abi Akindele <2366595+abbeymart@users.noreply.github.com>
2023-07-29 20:46:50 -0400

    refactor-module-refs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+#[path = "./res_types.rs"]
 mod res_types;
+#[path = "./res_std_messages.rs"]
 mod res_std_messages;
+#[path = "./res_message.rs"]
 mod res_message;
+#[path = "./net_status_code.rs"]
 mod net_status_code;
 
 #[cfg(test)]
